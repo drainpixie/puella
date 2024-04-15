9a73934dca7be549e2a4b1310273ad339ad71864
drainpixie <121581793+drainpixie@users.noreply.github.com>
2024-04-15 17:27:21 +0200

    feat: 2bwm actually working

--- hosts/timeline/xserver.nix
+++ hosts/timeline/xserver.nix
@@ -1,8 +1,18 @@
-_: {
+{ inputs, ... }: {
+  nixpkgs.overlays = [
+    (self: super: {
+      _2bwm = super._2bwm.overrideAttrs (_: {
+        src = inputs.wm;
+        patches = [ ../../patches/2bwm.diff ];
+      });
+    })
+  ];
+
   services.xserver = {
     enable = true;
 
     xkb.layout = "it";
+
     windowManager."2bwm".enable = true;
 
     displayManager = {
@@ -10,6 +20,8 @@ _: {
 
       autoLogin.enable = true;
       autoLogin.user = "akemi";
+
+      defaultSession = "none+2bwm";
     };
   };
 }
--- users/akemi/desktop.nix
+++ users/akemi/desktop.nix
@@ -1,12 +1,3 @@
-{ pkgs, wm, ... }: {
-  nixpkgs.overlays = [
-    (self: super: {
-      _2bwm = super._2bwm.overrideAttrs (_: {
-        src = wm;
-        patches = [ ../../patches/2bwm.diff ];
-      });
-    })
-  ];
-
+{ pkgs, inputs, ... }: {
   home.packages = with pkgs; [ rofi slock cozette tewi-font ];
 }
