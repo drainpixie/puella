cad9af850365df217890e1fa0a47d4af18486fa1
drainpixie <121581793+drainpixie@users.noreply.github.com>
2024-04-22 11:58:41 +0200

    feat: ok zsh sucks

--- common/cli.nix
+++ common/cli.nix
@@ -2,36 +2,30 @@ _: {
   programs = {
     direnv = {
       enable = true;
-      enableZshIntegration = true;
+      enableBashIntegration = true;
     };
 
     zoxide = {
       enable = true;
-      enableZshIntegration = true;
+      enableBashIntegration = true;
     };
 
     fzf = {
       enable = true;
-      enableZshIntegration = true;
+      enableBashIntegration = true;
     };
 
     eza = {
       enable = true;
-      enableZshIntegration = true;
+      enableBashIntegration = true;
 
-      extraOptions = [
-        "--group-directories-first"
-        "--sort=ext"
-      ];
+      extraOptions = [ "--group-directories-first" "--sort=ext" ];
     };
 
     ripgrep = {
       enable = true;
 
-      arguments = [
-        "--hidden"
-        "--smart-case"
-      ];
+      arguments = [ "--hidden" "--smart-case" ];
     };
 
     bat = {
@@ -42,76 +36,25 @@ _: {
       };
     };
 
-    # bash = {
-    #   enable = true;
-    #   enableCompletion = true;
-
-    #   historyControl = [
-    #     "ignorespace"
-    #     "erasedups"
-    #   ];
-    #   shellOptions = [
-    #     "histappend"
-    #     "cmdhist"
-    #     "lithist"
-    #     "histreedit"
-    #     "histverify"
-    #     "globstar"
-    #     "direxpand"
-    #     "cdspell"
-    #     "checkwinsize"
-    #     "dotglob"
-    #     "extglob"
-    #     "nocasematch"
-    #   ];
-
-    #   initExtra = ''
-    #     run() {
-    #         nix run nixpkgs#"$1" -- "''${@:2}"
-    #     }
-
-    #     __pwd() {
-    #       local pwd=$(dirs +0)
-
-    #       local components=$(echo "$pwd" | tr "/" "\n")
-    #       local count=$(echo "$components" | wc -l)
-
-    #       local idx=1
-    #       local short=""
-
-    #       for component in $components; do
-    #         if [ "$idx" -eq "$count" ]; then
-    #           short+="$component"
-    #         else
-    #           short+="''${component:0:1}/"
-    #         fi
-
-    #         ((idx++))
-    #       done
-
-    #       echo "$short"
-    #     }
-
-    #     LIGHT_BLUE="\[\033[1;34m\]"
-    #     LIGHT_GREEN="\[\033[1;31m\]"
-    #     NO_COLOR="\[\033[0m\]"
-
-    #     PS1="''${debian_chroot:+($debian_chroot)}''${LIGHT_GREEN}\u@\h''${NO_COLOR}:''${LIGHT_BLUE}\$(__pwd)''${NO_COLOR} \$ "
-    #   '';
-
-    #   bashrcExtra = ''
-    #     [[ -f ~/.profile ]] && . ~/.profile
-    #   '';
-    # };
-
-    zsh = {
+    bash = {
       enable = true;
       enableCompletion = true;
-      autocd = true;
 
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-      history.extended = true;
+      historyControl = [ "ignorespace" "erasedups" ];
+      shellOptions = [
+        "histappend"
+        "cmdhist"
+        "lithist"
+        "histreedit"
+        "histverify"
+        "globstar"
+        "direxpand"
+        "cdspell"
+        "checkwinsize"
+        "dotglob"
+        "extglob"
+        "nocasematch"
+      ];
 
       shellAliases = {
         # niceties
@@ -136,6 +79,44 @@ _: {
         # grep -> ripgrep
         grep = "rg --color=always --hidden --smart-case";
       };
+
+      initExtra = ''
+        run() {
+            nix run nixpkgs#"$1" -- "''${@:2}"
+        }
+
+        __pwd() {
+          local pwd=$(dirs +0)
+
+          local components=$(echo "$pwd" | tr "/" "\n")
+          local count=$(echo "$components" | wc -l)
+
+          local idx=1
+          local short=""
+
+          for component in $components; do
+            if [ "$idx" -eq "$count" ]; then
+              short+="$component"
+            else
+              short+="''${component:0:1}/"
+            fi
+
+            ((idx++))
+          done
+
+          echo "$short"
+        }
+
+        LIGHT_BLUE="\[\033[1;34m\]"
+        LIGHT_GREEN="\[\033[1;31m\]"
+        NO_COLOR="\[\033[0m\]"
+
+        PS1="''${debian_chroot:+($debian_chroot)}''${LIGHT_GREEN}\u@\h''${NO_COLOR}:''${LIGHT_BLUE}\$(__pwd)''${NO_COLOR} \$ "
+      '';
+
+      bashrcExtra = ''
+        [[ -f ~/.profile ]] && . ~/.profile
+      '';
     };
   };
 }
