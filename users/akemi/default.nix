3964bb8bc532d7c842646864fac73ad7e3178591
drainpixie <121581793+drainpixie@users.noreply.github.com>
2024-04-26 19:02:05 +0200

    feat: add bitwarden cli

--- users/akemi/default.nix
+++ users/akemi/default.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   imports = [
     ./neovim
     ./wezterm.nix
@@ -8,36 +7,31 @@
     ../../common/home.nix
   ];
 
-  home =
-    let
-      username = "akemi";
-    in
-    {
-      inherit username;
-      homeDirectory = "/home/${username}";
+  home = let
+    username = "akemi";
+  in {
+    inherit username;
+    homeDirectory = "/home/${username}";
 
-      packages = builtins.attrValues {
-        inherit (pkgs)
-          strace
-          man-pages
-          man-pages-posix
-
-          wget
-          curl
-          xclip
-
-          fd
-          tokei
-          du-dust
-
-          ungoogled-chromium
-          alacritty
-
-          zip
-          unzip
-          ;
-      };
+    packages = builtins.attrValues {
+      inherit
+        (pkgs)
+        strace
+        man-pages
+        man-pages-posix
+        wget
+        curl
+        xclip
+        fd
+        tokei
+        du-dust
+        ungoogled-chromium
+        zip
+        unzip
+        bitwarden-cli
+        ;
     };
+  };
 
   xdg.userDirs = {
     enable = true;
