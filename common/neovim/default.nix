63120b187ac7bd0abb85aa5118815767a6c22a69
drainpixie <121581793+drainpixie@users.noreply.github.com>
2024-06-12 21:19:51 +0200

    refactor(nvim): keybindings

--- common/neovim/default.nix
+++ common/neovim/default.nix
@@ -75,89 +75,73 @@
         action = ":%s/";
         key = "<leader>r";
         mode = "n";
-        options = {
-          desc = "Easier find and replace.";
-        };
+        options.desc = "Easier find and replace.";
       }
       {
         action = "<C-r>";
         key = "q";
         mode = "n";
-        options = {
-          desc = "Easier redo.";
-        };
+        options.desc = "Easier redo.";
       }
       {
         action = "<C-d>zz";
         key = "D";
         mode = "n";
-        options = {
-          desc = "Jump half a page without moving the cursor.";
-        };
+        options.desc = "Jump half a page without moving the cursor.";
       }
       {
         action = "<C-u>zz";
         key = "U";
         mode = "n";
-        options = {
-          desc = "Jump half a page without moving the cursor.";
-        };
+        options.desc = "Jump half a page without moving the cursor.";
       }
       {
         action = ":Telescope find_files<CR>";
         key = "<leader>ff";
         mode = "n";
-        options = {
-          desc = "Find file in project.";
-        };
+        options.desc = "Find file in project.";
+      }
+      {
+        action = ":Neogit kind=auto<CR>";
+        key = "<leader>g";
+        mode = "n";
+        options.desc = "Magit for Vim";
       }
       {
         action = "<Esc>";
         key = "kk";
         mode = "i";
-        options = {
-          desc = "Exit insert mode with 'kk'.";
-        };
+        options.desc = "Exit insert mode with 'kk'.";
       }
       {
         action = "<gv";
         key = "<";
         mode = "v";
-        options = {
-          desc = "Indent selected lines.";
-        };
+        options.desc = "Indent selected lines.";
       }
       {
         action = ">gv";
         key = ">";
         mode = "v";
-        options = {
-          desc = "Indent selected lines.";
-        };
+        options.desc = "Indent selected lines.";
       }
       {
         action = ":m '>+1<CR>gv=gv";
         key = "J";
         mode = "v";
-        options = {
-          desc = "Move selected lines down.";
-        };
+        options.desc = "Move selected lines down.";
       }
       {
         action = ":m '<-2<CR>gv=gv";
         key = "K";
         mode = "v";
-        options = {
-          desc = "Move selected lines up.";
-        };
+        options.desc = "Move selected lines up.";
       }
       {
         action = ":Oil<CR>";
         key = "-";
         mode = "n";
-        options = {
-          desc = "Open parent directory";
-        };
+        options.desc = "Open parent directory";
       }
     ];
 
