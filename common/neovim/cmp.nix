e690f788510442c86c0bc0da6b65967238bbf605
drainpixie <121581793+drainpixie@users.noreply.github.com>
2024-10-10 12:36:16 +0200

    feat(users/akemi): ✨ add alacritty

--- common/neovim/cmp.nix
+++ common/neovim/cmp.nix
@@ -108,37 +108,37 @@ _: {
     };
 
     extraConfigLua = ''
-          local cmp = require'cmp'
+       local cmp = require'cmp'
 
-          -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-          cmp.setup.cmdline({'/', "?" }, {
-         	 sources = {
-         		 { name = 'buffer' }
-         	 }
-          })
+       -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+       cmp.setup.cmdline({'/', "?" }, {
+      	 sources = {
+      		 { name = 'buffer' }
+      	 }
+       })
 
-         -- Set configuration for specific filetype.
-          cmp.setup.filetype('gitcommit', {
-         	 sources = cmp.config.sources({
-         		 { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-         	 }, {
-         		 { name = 'buffer' },
-         	 })
-          })
+      -- Set configuration for specific filetype.
+       cmp.setup.filetype('gitcommit', {
+      	 sources = cmp.config.sources({
+      		 { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+      	 }, {
+      		 { name = 'buffer' },
+      	 })
+       })
 
-          -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-          cmp.setup.cmdline(':', {
-         	 sources = cmp.config.sources({
-         		 { name = 'path' }
-         	 }, {
-         		 { name = 'cmdline' }
-         	 })
-          })
+       -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+       cmp.setup.cmdline(':', {
+      	 sources = cmp.config.sources({
+      		 { name = 'path' }
+      	 }, {
+      		 { name = 'cmdline' }
+      	 })
+       })
 
-      require("copilot").setup({
-      	suggestion = { enabled = false },
-      	panel = { enabled = false },
-      })
+       require("copilot").setup({
+         suggestion = { enabled = false },
+        panel = { enabled = false },
+       })
     '';
   };
 }
--- users/akemi/terminal.nix
+++ users/akemi/terminal.nix
@@ -32,6 +32,66 @@
 
       scroll.bar.enable = false;
     };
+
+    alacritty = {
+      enable = true;
+      settings = {
+        window = {
+          padding = {
+            x = 16;
+            y = 16;
+          };
+        };
+        font = {
+          normal = {
+            family = "azuki_font";
+          };
+          size = 12;
+        };
+
+        keyboard = {
+          bindings = [
+            {
+              key = "C";
+              mods = "Control|Shift";
+              action = "Copy";
+            }
+            {
+              key = "V";
+              mods = "Control|Shift";
+              action = "Paste";
+            }
+          ];
+        };
+
+        colors = {
+          primary = {
+            background = "0xf7f7f7";
+            foreground = "0x000000";
+          };
+          normal = {
+            black = "0x282a2e";
+            red = "0xaa3731";
+            green = "0x448c27";
+            yellow = "0xcb9000";
+            blue = "0x325cc0";
+            magenta = "0x7a3e9d";
+            cyan = "0x0083b2";
+            white = "0x707880";
+          };
+          bright = {
+            black = "0x373b41";
+            red = "0xf05050";
+            green = "0x60cb00";
+            yellow = "0xffbc5d";
+            blue = "0x007acc";
+            magenta = "0xe64ce6";
+            cyan = "0x00aacb";
+            white = "0xc5c8c6";
+          };
+        };
+      };
+    };
   };
 
   home.packages = builtins.attrValues {
